98feb90c4f3187652d71369f9662633f772eaac0
Marc <ninjaz.gg@outlook.com>
2023-10-17 00:42:37 -0400

    move book_handler back to 'handlers' package

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -4,11 +4,12 @@ import (
 	"database/sql"
 
 	"github.com/umizu/yomu/internal/data"
+	"github.com/umizu/yomu/internal/handlers"
 )
 
 func (s *APIServer) RegisterBookRoutes(db *sql.DB) {
 
-	bookHandler := NewBookHandler(data.NewPostgresBookStore(db))
+	bookHandler := handlers.NewBookHandler(data.NewPostgresBookStore(db))
 	s.router.GET("/books", bookHandler.BooksGETHandler)
 	s.router.POST("/books", bookHandler.BooksPOSTHandler)
 }
