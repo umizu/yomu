71842949af670e16f0becbe9731b7418ddb93320
Marc <ninjaz.gg@outlook.com>
2023-11-04 18:16:39 -0400

    map LibraryItem from contract to domain

--- internal/handlers/library_item_handler.go
+++ internal/handlers/library_item_handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/umizu/yomu/internal/data"
+	"github.com/umizu/yomu/internal/types"
 	"github.com/umizu/yomu/pkg/contracts"
 )
 
@@ -35,5 +36,10 @@ func (h *LibraryItemHandler) LibraryItemPOSTHandler(c echo.Context) error {
 	if err := req.Validate(); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]string{"error": err.Error()})
 	}
-	return c.JSON(http.StatusCreated, nil)
+
+	libraryItem := types.NewLibraryItemFromRequest(req)
+	if err := h.libraryItemStore.Create(libraryItem); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, libraryItem)
 }
--- internal/types/library_item.go
+++ internal/types/library_item.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"github.com/google/uuid"
+	"github.com/umizu/yomu/pkg/contracts"
+)
+
 type LibraryItem struct {
 	Id     string `json:"id"`
 	BookId string `json:"bookId"`
@@ -14,3 +19,41 @@ const (
 	Completed
 	Dropped
 )
+
+func NewLibraryItemFromRequest(req contracts.CreateLibraryItemRequest) *LibraryItem {
+	return &LibraryItem{
+		Id:   uuid.NewString(),
+		BookId: req.BookId,
+		Status: ParseStatus(req.Status),
+	}
+}
+
+func ParseStatus(status string) Status {
+	switch status {
+	case "Planning":
+		return Planning
+	case "Reading":
+		return Reading
+	case "Completed":
+		return Completed
+	case "Dropped":
+		return Dropped
+	default:
+		return Planning
+	}
+}
+
+func (s Status) String() string {
+	switch s {
+	case Planning:
+		return "Planning"
+	case Reading:
+		return "Reading"
+	case Completed:
+		return "Completed"
+	case Dropped:
+		return "Dropped"
+	default:
+		return ""
+	}
+}
